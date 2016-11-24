dae09649a45667845a0e6dbd553cf2a89af84cad
1505669 <1505669@Y348SDP20.ad.dundeeandangus.ac.uk>
2016-11-24 10:24:15 +0000

    3.0: ROT13

--- src/firstapp/FirstApp.java
+++ src/firstapp/FirstApp.java
@@ -15,13 +15,45 @@ public class FirstApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+
         // TODO code application logic here
         
+         String encryptedValue="";
+         String input="Becky Esplin";
         // a few changes here
         
         // another few changes
         
         System.out.print("First App");
+        encryptedValue=encrypt(input, encryptedValue);
+        System.out.println("\n"+encryptedValue);
+    }
+    
+    public static String encrypt(String input, String encryptedValue){
+    for(int i=0;i<input.length(); i++)
+    {
+        char c = input.charAt(i);
+        if(c >= 'a' && c <='m')
+        {
+            c+=13;
+        }
+        else if(c >= 'A' && c <='M')
+                {
+                    c+=13;
+                }
+    else if(c >= 'n' && c <='z')
+                {
+                    c-=13;
+                }
+        else if(c >= 'N' && c <='Z')
+                {
+                    c-=13;
+                }
+        encryptedValue = encryptedValue+c;
+    }
+        
+    return encryptedValue;
     }
     
 }
