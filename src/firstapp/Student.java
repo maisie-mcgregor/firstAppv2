86ff851814cb3e612df1878d2ed3c1a3cc8271ef
1505669 <1505669@Y348SDP20.ad.dundeeandangus.ac.uk>
2016-11-24 10:03:54 +0000

    2.0: Added Class

---
+++ src/firstapp/Student.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapp;
+
+/**
+ *
+ * @author 1505669
+ */
+public class Student {
+    int age;
+    double height;
+    String firstName;
+    String lastName;
+    
+    public Student(age, height, firstName, lastName){
+    this.age = age;
+    this.height = height;
+    this.firstName = firstName;
+    this.lastname = lastName;
+    }
+}
